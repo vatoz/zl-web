07a980da73847d8300641262d79ab0b4f9414c1d
Václav Černý <vatoz@seznam.cz>
2017-06-24 10:27:08 +0200

    Dokumentace stávajícího stavu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Načte do pravého ČI LEVÉHO  bloku odkaz z lokálního webu, POKUD JE TO VPRAVO TAK PŘIDÁ TŘÍDU YELLOW
 function loadTo(href,left){
    jQuery.ajax({
     url: href,
@@ -29,12 +30,13 @@ function loadTo(href,left){
 }
 
 
+//zobrazí menu
 function ShowMenu(){
     jQuery("div.menu").css("display","block");
     return false;
 }
 
-
+//zjišťuje, zda je odkaz místní (pro ostrou, vývojovou a lokální verzi)
 function isLocal(href){
     if(href.indexOf("zlin.cz")>0) return true;
     if(href.indexOf("j.ntm.cz")>0) return true;
@@ -42,31 +44,34 @@ function isLocal(href){
     return false;
 }
 
+//po načtení dokumentu provede
 jQuery(document).ready(function(){
+   //pravé menu načte výstupy  
    jQuery(".menu a").live('click',function(e){       
         href=jQuery(this).attr('href');
-        console.log(href);
         if( isLocal(href)){
-           
            loadTo(href,false);
-            
            return false;            
         }                   
     });
     
-     jQuery(".menu0 a").live('click',function(e){       
+    //levé menu načte obsah a zmizí
+    jQuery(".menu0 a").live('click',function(e){       
            hideMenu(); 
-           return true;            
-                           
+           return true;                                       
     });
+    
+    
 });
+
+//okamžitě skryje menu a po chvilce ho zase obnoví. takhel už nebudeme hover na něm
 function hideMenu(){
     jQuery(".menu0 ul,  ul.menu").css("display","none");
     setTimeout(function()
     {    jQuery(".menu0 ul, ul.menu").css("display","block");},200);
 }
 
-
+//rozbalí blok číst dál.
 function Unfade(id){
     jQuery("#overlay_"+id).fadeTo(30,0,function(){
         jQuery("#overlay_"+id).remove();
@@ -80,9 +85,8 @@ function Unfade(id){
         jQuery("#fade_"+id).replaceWith(jQuery("#content_"+id).html() );
                    jQuery("#hide_"+id).remove(); 
                 }
-                )
-        
-        
+                );
+         
     });
     
 }
